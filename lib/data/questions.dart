2a3185d96e4b75586ab4e13ad44949baa6ab0e09
ChrisCapital <chris.shavuo@gmail.com>
2026-02-12 11:44:27 -0500

    finished making questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('How did Edward Elric Lose his arm in FMA?', ['Putting his brothers soul in a suit of armor','In a Duel', 'In a freak Dabbing Accident', 'He Never had arms']),
+  QuizQuestion('What city is on the east coast',['New York','San Francisco','Los Angeles', 'Seattle']),
+  QuizQuestion('what place has the clearest water',['Caribbean','west coast','east coast', 'Hawaii']),
+  QuizQuestion('Whats the most popular sport global',['Futbol','Football', 'Lacrosse','baseball']),
+  QuizQuestion('What is the time complexity of binary search on a sorted array?',['O(log n)','O(n log n)',' O(n)','O(1)']),
+  QuizQuestion('Which data structure uses a Last-In, First-Out (LIFO) principle?',['Stack','Queue', 'Heap','Linked List'])
+];
