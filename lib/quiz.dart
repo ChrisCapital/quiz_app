8af4b539775fe27f3c725879bbe2ba906c995257
ChrisCapital <chris.shavuo@gmail.com>
2026-02-12 11:59:12 -0500

    updated style to be in quiz widget

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,30 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
 
-
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz>{
-  Widget ? activeScreen;
-  void switchScreen(){
-    setState((){
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+  void switchScreen() {
+    setState(() {
       activeScreen = QuestionsScreen();
     });
   }
-  
-  void initState(){
+
+  void initState() {
     activeScreen = StartScreen(switchScreen);
     super.initState();
   }
-  
-  @override 
-  Widget build(BuildContext context){
-    return activeScreen!;
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 63, 8, 165),
+                Color.fromARGB(255, 12, 1, 39),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
   }
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,44 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
-import 'package:quiz_app/questions_screen.dart';
-
 
 class StartScreen extends StatelessWidget {
   const StartScreen(
-    this.startQuiz,{super.key});
+    this.startQuiz, {super.key});
   final void Function() startQuiz;
   @override
-  Widget build(BuildContext context){
-
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 78, 8, 165),
-            Color.fromARGB(255, 12, 1, 39),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Center(
+  Widget build(BuildContext context) {
+    return Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-              Image.asset('assets/images/quiz-logo.png',width: 280,color: Color.fromARGB(200,255,255,255)),
-              const SizedBox(height : 80),
-                Text(
-                  "challege yourself on this quiz",
-                  style: TextStyle(color:Colors.white, fontSize:22)),
-                const SizedBox(height:30),
-                OutlinedButton.icon(
-                  onPressed: startQuiz,
-                  icon: const Icon(Icons.arrow_circle_right_outlined),
-                  style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-                  label: Text("Start Quiz")
-                )
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 280,
+              color: Color.fromARGB(200, 255, 255, 255),
+            ),
+            const SizedBox(height: 80),
+            Text(
+              "Challenge yourself on this quiz!",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton.icon(
+              onPressed:startQuiz,
+              icon: const Icon(Icons.arrow_circle_right_outlined),
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              label: Text("Start Quiz"),
+            ),
           ],
         ),
-      ),
-    );
+      );
   }
 }
