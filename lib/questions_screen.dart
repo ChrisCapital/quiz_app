6afaa3b55d1ed43782a88476ccad0a9ca50276e6
ChrisCapital <chris.shavuo@gmail.com>
2026-02-12 13:22:54 -0500

    adding questions to the screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -12,14 +15,25 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   
   @override
   Widget build(BuildContext context){
-    return Column(
-      children: [
-        const Text('This is the question')],
-        const SizedBox(height:30),
-        ElevatedButton(onPressed: (){},child: const Text('Answer 1')),
-        ElevatedButton(onPressed: (){},child: const Text('Answer 2')),
-        ElevatedButton(onPressed: (){},child: const Text('Answer 3')),
-        ElevatedButton(onPressed: (){},child: const Text('answer 4')),
-    )
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.question,
+          style:TextStyle(
+            color:Colors.white,
+            fontSize:20
+          ),
+         ),
+          const SizedBox(height:30),
+          AnswerButton(answerText:currentQuestion.answers[0], onTap: (){}),
+          AnswerButton(answerText:currentQuestion.answers[1], onTap: (){}),
+          AnswerButton(answerText:currentQuestion.answers[2], onTap: (){}),
+          AnswerButton(answerText:currentQuestion.answers[3], onTap: (){}),
+      ],),
+    );
   }
 }
