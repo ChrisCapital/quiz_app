3cd9e03be7571ec53b6b8a602150b671b96f9b12
ChrisCapital <chris.shavuo@gmail.com>
2026-02-19 12:35:26 -0500

    added shuffled answers logic

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,13 @@ class QuizQuestion{
 
   final String question;
   final List<String> answers;
+
+
+
+  // This method creates a new list of answers, shuffles it, and returns the shuffled list.
+  List<String>getShuffledAnswers(){
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
